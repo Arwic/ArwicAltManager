b205c312190d1bd9ebf88dca3e7c918f5877b5d3
Tim <tim@tim-ings.com>
2018-03-21 03:16:29 +0800

    Adds general tab in row mode

--- ArwicAltManager.lua
+++ ArwicAltManager.lua
@@ -54,6 +54,11 @@ local ridingPathfinderIDs = {
 local inventorySlotIDs = {
     1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17
 }
+local genderMap = {
+    [1] = "Unknown",
+    [2] = "Male",
+    [3] = "Female",
+}
 
 function InitDB()
     if ArwicAltManagerDB == nil then 
@@ -195,3 +200,132 @@ function AAM_Init()
 end
 
 AAM_Init()
+
+-----------------------------------------------
+
+local function NewLabel(parent, fontHeight, text)
+    local str = parent:CreateFontString()
+    str:SetParent(parent)
+    str:SetFont("fonts/ARIALN.ttf", fontHeight)
+    str:SetText(text)
+    return str
+end
+
+function AAM_BuildGeneralFrame()
+    -- dont remake the frame if it already exists
+    if AAM_mainFrame ~= nil then return end
+
+    AAM_mainFrame = CreateFrame("Frame", "AAM_mainFrame", UIParent)
+    AAM_mainFrame:SetFrameStrata("HIGH")
+    AAM_mainFrame:SetWidth(850)
+    AAM_mainFrame:SetHeight(600)
+    AAM_mainFrame:SetPoint("CENTER",0,0)
+
+    AAM_mainFrame.texture = AAM_mainFrame:CreateTexture(nil, "BACKGROUND")
+    AAM_mainFrame.texture:SetColorTexture(0.1, 0.1, 0.1, 0.9)
+    AAM_mainFrame.texture:SetAllPoints(AAM_mainFrame)
+
+    local charCount = 0
+    local rowWidth = AAM_mainFrame:GetWidth()
+    local rowHeight = 20
+    local headerheight = 20
+    
+    -- create the header
+    local headerCols = {
+        "Name",
+        "Class",
+        "Realm",
+        "Faction",
+        "Race",
+        "Gender",
+        "Level",
+        "Riding Speed",
+    }
+    local headerRowFrame = CreateFrame("FRAME", "AAM_headerRowFrame", AAM_mainFrame)
+    headerRowFrame:SetWidth(rowWidth)
+    headerRowFrame:SetHeight(rowHeight)
+    headerRowFrame:SetPoint("TOPLEFT", 0, 0)
+    headerRowFrame.texture = headerRowFrame:CreateTexture(nil, "BACKGROUND")
+    headerRowFrame.texture:SetColorTexture(0.15, 0.15, 0.3, 1.0)
+    headerRowFrame.texture:SetAllPoints(headerRowFrame)
+    local widthSoFar = 0
+    local headerSep = 5
+    local colWidth = 90
+    local headerFontHeight = 20
+    for _, colName in pairs(headerCols) do
+        local str = headerRowFrame:CreateFontString()
+        str:SetParent(headerRowFrame)
+        str:SetFont("fonts/ARIALN.ttf", headerFontHeight)
+        str:SetText(colName)
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+    end
+
+    for k, v in pairs(ArwicAltManagerDB.Realms[charRealm]) do
+        -- create the row frame
+        local charRowFrame = CreateFrame("FRAME", "AAM_charRowFrame_" .. v.Realm .. "_" .. v.Name, AAM_mainFrame)
+        charRowFrame:SetWidth(rowWidth)
+        charRowFrame:SetHeight(rowHeight)
+        charRowFrame:SetPoint("TOPLEFT", 0, -(rowHeight * charCount + headerheight))
+        -- give the row a texture
+        if charCount % 2 == 0 then
+            charRowFrame.texture = charRowFrame:CreateTexture(nil, "BACKGROUND")
+            charRowFrame.texture:SetColorTexture(0.15, 0.15, 0.15, 1.0)
+            charRowFrame.texture:SetAllPoints(charRowFrame)
+        end
+        
+        local char = CharData(v.Name, v.Realm)
+        local classColorR, classColorG, classColorB = 
+            RAID_CLASS_COLORS[char.Class].r, 
+            RAID_CLASS_COLORS[char.Class].g, 
+            RAID_CLASS_COLORS[char.Class].b
+        local dataFontHeight = 15
+        local widthSoFar = 0
+
+        -- name
+        local str = NewLabel(charRowFrame, dataFontHeight, char.Name)
+        str:SetTextColor(classColorR, classColorG, classColorB)
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+        -- class 
+        local className = char.Class:sub(1,1):upper() .. char.Class:sub(2):lower()
+        if className == "Demonhunter" then className = "Demon Hunter" end
+        if className == "Deathknight" then className = "Death Knight" end
+        local str = NewLabel(charRowFrame, dataFontHeight, className)
+        str:SetTextColor(classColorR, classColorG, classColorB)
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+        -- realm
+        local str = NewLabel(charRowFrame, dataFontHeight, char.Realm)
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+        -- faction
+        local str = NewLabel(charRowFrame, dataFontHeight, char.Faction)
+        if char.Faction == "Alliance" then
+            str:SetTextColor(0, 0.8, 1)
+        elseif char.Faction == "Horde" then
+            str:SetTextColor(0.85, 0, 0)
+        end
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+        -- race
+        local str = NewLabel(charRowFrame, dataFontHeight, char.Race)
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+        -- gender 
+        local str = NewLabel(charRowFrame, dataFontHeight, genderMap[char.Gender])
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+        -- level
+        local str = NewLabel(charRowFrame, dataFontHeight, char.Level)
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+        -- mount Speed
+        local str = NewLabel(charRowFrame, dataFontHeight, char.MountSpeed .. "%")
+        str:SetPoint("LEFT", widthSoFar + headerSep, 0)
+        widthSoFar = widthSoFar + colWidth + headerSep
+
+        -- increment the counter
+        charCount = charCount + 1
+    end
+end
